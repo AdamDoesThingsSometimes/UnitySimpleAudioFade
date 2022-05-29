4f7a43972e30a1a6985b6fa28240561fbf3fb866
AdamDoesThingsSometimes <105390994+AdamDoesThingsSometimes@users.noreply.github.com>
2022-05-29 09:56:21 +0100

    Initial Commit
    Made a script!

---
+++ AudioFader.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioFader : MonoBehaviour
+{
+    //A nice friendly fader! Make sure to pop this on a gameobject with an AudioSource in order have it fade
+    //I toyed with the idea of making this script inherit from AudioSource instead of Monobehaviour, but that seemed like it could be annoying! :)
+
+    [Tooltip("The audio source we're fading")]
+    AudioSource audioSource;
+
+    [Header("Default fading time")]
+    //Change these numbers to different limits if they aren't what you need! :)
+    [Range(0.01f, 5f)]
+    public float defaultFadeLength = 1;
+
+    private void Start()
+    {
+        if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    /// <summary>
+    /// Nice and easy way to fade in
+    /// </summary>
+    [ContextMenu("Fade In")]
+    public void FadeIn()
+    {
+        FadeTo(1);
+    }
+
+    /// <summary>
+    /// Nice easy way to fade in, but with a custom time that it takes to fade in
+    /// </summary>
+    /// <param name="customFadeLength">The time you'd like it to take when fading out</param>
+    public void FadeIn(float customFadeLength)
+    {
+        FadeTo(1, customFadeLength);
+    }
+
+    /// <summary>
+    /// Nice and easy way to fade out
+    /// </summary>
+    [ContextMenu("Fade Out")]
+    public void FadeOut()
+    {
+        FadeTo(0);
+    }
+
+    /// <summary>
+    /// Nice and easy way to fade out, but with a custom time that it takes to fade out
+    /// </summary>
+    /// <param name="customFadeLength">The time you'd like to to take when fading out</param>
+    public void FadeOut(float customFadeLength)
+    {
+        FadeTo(0, customFadeLength);
+    }
+
+    /// <summary>
+    /// Fades your audiosource to <paramref name="targetLevel"/> using the setting for defaultFadeLength set in the inspector
+    /// </summary>
+    /// <param name="targetLevel">The level you want to set your audio to</param>
+    public void FadeTo(float targetLevel)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeToCo(targetLevel, defaultFadeLength));
+    }
+
+    /// <summary>
+    /// Fades your audiosource to <paramref name="targetLevel"/> using a custom time you pass in using <paramref name="customFadeLength"/>
+    /// </summary>
+    /// <param name="targetLevel">The level you want to set your audio to</param>
+    /// <param name="customFadeLength">The time it takes to transition to targetLevel</param>
+    public void FadeTo(float targetLevel, float customFadeLength)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeToCo(targetLevel, customFadeLength));
+    }
+
+
+    /// <summary>
+    /// The main fade coroutine - use the voids to call this, as they handle stopping this function so that you don't get 2 fades on the go at once!
+    /// </summary>
+    /// <param name="targetLevel">The the level to fade to</param>
+    /// <param name="fadeLength">The time it takes for it to fade</param>
+    /// <returns></returns>
+    IEnumerator FadeToCo(float targetLevel, float fadeLength)
+    {
+#if UNITY_EDITOR
+        if (Application.isPlaying == false)
+        {
+            Debug.Log("Can't use fading unless you're in play mode!");
+            yield break;
+        }
+#endif
+
+        float currentTime = 0;
+        float start = audioSource.volume;
+        
+        while (currentTime < fadeLength)
+        {
+            //Add to the currentTime the amount of time between this frame and the last one, then we use that to make a percentage against the total time
+            currentTime += Time.deltaTime;
+
+            //Imagine a line, lerp is "Here are two numbers - start and targetLevel, what percent between those two numbers do we need
+            //If you put in start, targetLevel, 0.1 - you'd get 10% of the way between the two, put 1 in and you'd get targetLevel immediately
+            //By using currentTime that we're adding to against the total seconds, we get that percentage!
+            audioSource.volume = Mathf.Lerp(start, targetLevel, currentTime / fadeLength);
+            yield return null;
+        }
+
+        audioSource.volume = targetLevel;
+
+        yield break;
+    }
+}
